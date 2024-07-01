9e9c43c599c28936e2b0c04673d4682c1925d72c
AkhdaanLogics <akhdaan@students.amikom.ac.id>
2024-07-01 21:09:51 +0700

    Add project files.

---
+++ pertemuan_13/Mahasiswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System;
+
+public class Mahasiswa
+{
+    public string Nama { get; set; }
+    public int Nim { get; set; }
+    public int Nilai { get; set; }
+
+    // Constructor
+    public Mahasiswa(int nim, string nama, int nilai)
+    {
+        Nim = nim;
+        Nama = nama;
+        Nilai = nilai;
+    }
+
+    // Method untuk mencetak informasi mahasiswa
+    public void cetakData(int nomor)
+    {
+        Console.WriteLine(nomor + "\t" + Nim + "\t" + Nama + "\t" + Nilai + "\t");
+    }
+}
+
---
+++ pertemuan_13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Mahasiswa[] mahasiswaArr = new Mahasiswa[3];
+
+        mahasiswaArr[0] = new Mahasiswa(1234, "Erik", 75);
+        mahasiswaArr[1] = new Mahasiswa(1235, "Wely", 80);
+        mahasiswaArr[2] = new Mahasiswa(1236, "Sony", 95);
+
+        Console.WriteLine("No.\tNim\tNama\tNilai");
+        Console.WriteLine("==============================");
+
+        int nomor = 1;
+        foreach (var mahasiswa in mahasiswaArr)
+        {
+            mahasiswa.cetakData(nomor);
+            nomor++;
+        }
+    }
+}
+
+
